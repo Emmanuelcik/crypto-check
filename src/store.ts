82838d641147c51b08a379fb0a130f28cb709866
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-07-10 23:59:48 -0600

    get all cryptos from the API

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,26 @@
 import { create } from "zustand";
 import axios from "axios";
+import { CryptoCurrenciesResponseSchema } from "./schema/crypto-schema";
+import { CryptoCurrency } from "./types";
+
+type CryptoStore = {
+  cryptoCurrencies: CryptoCurrency[];
+  fetchCryptos: () => Promise<void>;
+};
 const getCryptos = async () => {
   const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`;
   const {
     data: { Data },
   } = await axios(url);
-  console.log(Data);
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data);
+  if (result.success) {
+    return result.data;
+  }
 };
-export const useCryptoStore = create(() => ({
-  fetchCryptos: () => {
-    getCryptos();
+export const useCryptoStore = create<CryptoStore>((set) => ({
+  cryptoCurrencies: [],
+  fetchCryptos: async () => {
+    const cryptoCurrencies = await getCryptos();
+    set(() => ({ cryptoCurrencies }));
   },
 }));
