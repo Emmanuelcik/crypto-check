f038e4f128c1a2dbead43cff1cc631a3bb07930e
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-07-11 00:09:43 -0600

    populate cryptocurrencies select from the api

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,8 @@
 import { currencies } from "../data";
+import { useCryptoStore } from "../store";
 
 const SearchForm = () => {
+  const cryptCurrencies = useCryptoStore((state) => state.cryptoCurrencies);
   return (
     <form className="form">
       <div className="field">
@@ -18,6 +20,11 @@ const SearchForm = () => {
         <label htmlFor="crytocurrency">Cryptocurrency: </label>
         <select name="crytocurrency" id="crytocurrency">
           <option>--Select--</option>
+          {cryptCurrencies.map((currency) => (
+            <option key={currency.CoinInfo.Name} value={currency.CoinInfo.Name}>
+              {currency.CoinInfo.FullName}
+            </option>
+          ))}
         </select>
       </div>
 
