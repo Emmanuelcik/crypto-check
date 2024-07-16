90119cea8c8fa99b8ef68fcddb789448f0f0eb5e
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-07-15 22:51:58 -0600

    handle state and form validation

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,5 @@
+const ErrorMessage = ({ children }: React.PropsWithChildren) => {
+  return <div>{children}</div>;
+};
+
+export default ErrorMessage;
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import { currencies } from "../data";
 import { useCryptoStore } from "../store";
+import { Pair } from "../types";
+import ErrorMessage from "./ErrorMessage";
 
 const SearchForm = () => {
   const cryptCurrencies = useCryptoStore((state) => state.cryptoCurrencies);
+
+  const initialState = {
+    currency: "",
+    cryptoCurrency: "",
+  };
+  const [pair, setPair] = useState<Pair>(initialState);
+  const [error, setError] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPair({ ...pair, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (Object.values(pair).includes("")) {
+      setError("Please select both a currency and a cryptocurrency");
+      return;
+    }
+    setError("");
+  };
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
       <div className="field">
         <label htmlFor="currency">Currency: </label>
-        <select name="currency" id="currency">
+        <select
+          name="currency"
+          id="currency"
+          onChange={handleChange}
+          value={pair.currency}
+        >
           <option>--Select--</option>
           {currencies.map((currency) => (
             <option key={currency.code} value={currency.code}>
@@ -17,8 +47,13 @@ const SearchForm = () => {
         </select>
       </div>
       <div className="field">
-        <label htmlFor="crytocurrency">Cryptocurrency: </label>
-        <select name="crytocurrency" id="crytocurrency">
+        <label htmlFor="cryptoCurrency">Cryptocurrency: </label>
+        <select
+          name="cryptoCurrency"
+          id="cryptoCurrency"
+          onChange={handleChange}
+          value={pair.cryptoCurrency}
+        >
           <option>--Select--</option>
           {cryptCurrencies.map((currency) => (
             <option key={currency.CoinInfo.Name} value={currency.CoinInfo.Name}>
